5e39c11551731b6055a341b1b39667834d03a0b1
AnnGular <andrea.aguilar@ingenieria.uner.edu.ar>
2025-03-26 13:01:28 -0300

    Agregue funcion ordenamiento por burbuja

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,19 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
+import random
+def bubble_sort(lista):
+    n=len(lista)
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if lista[j]>lista[j+1]:
+                lista[j], lista[j+1]=lista[j+1], lista[j]
+    return lista
+
+numeros=[]
+for i in range(500):
+    
+    x=random.randint(10000,20000)
+    numeros.append(x)
+    
+print(numeros)
+
+bubble_sort(numeros)
+print(f"ACA EMPIEZA: {numeros}")
