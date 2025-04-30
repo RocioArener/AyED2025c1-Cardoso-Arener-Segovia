934432e57993e1e349c50969cda36726fb7ff665
LucasSegovia01 <lukassjg06@gmail.com>
2025-04-30 11:21:30 -0300

    Cambios en modulo 1 de proyecto 2

--- TrabajoPractico_1/proyecto_2/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_2/modules/modulo1.py
@@ -1,186 +1,191 @@
 class Nodo:
     def __init__(self, dato):
-        self.dato=dato
-        self.siguiente=None
-        self.anterior=None
+        self._dato = dato
+        self._siguiente = None
+        self._anterior = None
+
+    @property
+    def dato(self):
+        return self._dato
+    
+    @dato.setter
+    def dato(self, valor):
+        self._dato = valor
+
+    @property
+    def siguiente(self):
+        return self._siguiente
+    
+    @siguiente.setter
+    def siguiente(self, valor):
+        self._siguiente = valor
+        
+    @property
+    def anterior(self):
+        return self._anterior
+    
+    @anterior.setter
+    def anterior(self, valor):
+        self._anterior = valor
 
 class ListaDobleEnlazada:
     def __init__(self):
-        self.cabeza=None
-        self.cola=None
-        self.size=0
-
+        self._cabeza = None
+        self._cola = None
+        self._tamanio = 0
+        
     def __iter__(self):
-        nodo_actual = self.cabeza
+        nodo_actual = self._cabeza
         while nodo_actual is not None:
             yield nodo_actual.dato
             nodo_actual = nodo_actual.siguiente
 
-    def esta_vacia(self):
-        return self.cabeza==None
+    @property
+    def cabeza(self):
+        return self._cabeza
     
-    def __len__(self):
-        return self.size
+    @cabeza.setter
+    def cabeza(self, valor):
+        self._cabeza = valor
+    
+    @property
+    def cola(self):
+        return self._cola
+    
+    @cola.setter
+    def cola(self, valor):
+        self._cola = valor
         
+    @property
+    def tamanio(self):
+        return self._tamanio
+    
+    @tamanio.setter
+    def tamanio(self, valor):
+        self._tamanio = valor
+
+    def esta_vacia(self):
+        return self._cabeza is None
+
+    def __len__(self):
+        return self._tamanio
+
     def agregar_al_final(self, dato):
-        nuevo=Nodo(dato)
+        nuevo = Nodo(dato)
         if self.esta_vacia():
-            self.cabeza =self.cola=nuevo
+            self._cabeza = self._cola = nuevo
         else:
-            nuevo.anterior=self.cola
-            self.cola.siguiente=nuevo
-            self.cola=nuevo
-            self.cola.siguiente=None
-        self.size+=1
+            nuevo.anterior = self._cola
+            self._cola.siguiente = nuevo
+            self._cola = nuevo
+        self._tamanio += 1
 
     def agregar_al_inicio(self, dato):
-        nuevo=Nodo(dato)
+        nuevo = Nodo(dato)
         if self.esta_vacia():
-            self.cabeza=self.cola=nuevo
+            self._cabeza = self._cola = nuevo
         else:
-            nuevo.siguiente=self.cabeza
-            self.cabeza.anterior=nuevo
-            self.cabeza=nuevo
-            self.cabeza.anterior=None
-        self.size+=1
-    
-    def insertar(self, item, posicion=None):
+            nuevo.siguiente = self._cabeza
+            self._cabeza.anterior = nuevo
+            self._cabeza = nuevo
+        self._tamanio += 1
+
+    def insertar(self, dato, posicion=None):
         if posicion is None:
-            return self.agregar_al_final(item)
-        if posicion < 0 or posicion > self.size:
+            return self.agregar_al_final(dato)
+        if posicion < 0 or posicion > self._tamanio:
             raise IndexError("Posición inválida")
         if posicion == 0:
-            return self.agregar_al_inicio(item)
-        elif posicion == self.size:
-            return self.agregar_al_final(item)
+            return self.agregar_al_inicio(dato)
+        elif posicion == self._tamanio:
+            return self.agregar_al_final(dato)
         else:
-            nuevo = Nodo(item)
-            actual = self.cabeza
-            # Avanzamos hasta la posición anterior a donde insertaremos
+            nuevo = Nodo(dato)
+            actual = self._cabeza
             for _ in range(posicion - 1):
                 actual = actual.siguiente
-            # Conectamos el nuevo nodo
             nuevo.siguiente = actual.siguiente
             nuevo.anterior = actual
             actual.siguiente.anterior = nuevo
             actual.siguiente = nuevo
-            self.size += 1
-            
+            self._tamanio += 1
+
     def extraer(self, posicion=None):
         if self.esta_vacia():
             raise Exception("Lista vacía")
-        if posicion == self.size or posicion==None:
-            posicion=self.size -1   
-        if posicion < 0 or posicion > self.size:
-            raise Exception("Posición inválida")
-        #Primer elemento
-        if posicion ==0:
-            nodo=self.cabeza
-            self.cabeza=nodo.siguiente
-            if self.cabeza:
-                self.cabeza.anterior= None
-            else:
-                self.cola= None
-            self.size-=1
-            return nodo.dato
-        #Ultimo elemento
-        elif posicion == self.size-1:
-            nodo=self.cola
-            self.cola =nodo.anterior
-            if self.cola:
-                self.cola.siguiente=None
-            else:
-                self.cabeza = None
-            self.size-=1
-            return nodo.dato
-        #Cualquier otro elemento
-        else:
-            actual=self.cabeza
-            for i in range(posicion):
-                actual= actual.siguiente
-            actual.anterior.siguiente=actual.siguiente
-            if actual.siguiente:
-                actual.siguiente.anterior=actual.anterior
-                actual.anterior.siguiente = actual.siguiente
-                self.size-=1
-            return actual.dato
         
+        if posicion is None:
+            posicion = self._tamanio - 1
+        elif posicion < 0:
+            posicion=self._tamanio+posicion
+            if posicion <0: 
+                 raise Exception("Posición inválida")
+            
+        if posicion <0 or posicion>=self._tamanio:
+            raise Exception("Posición inválida")
+        
+        if posicion == 0:
+            nodo = self._cabeza
+            self._cabeza = nodo.siguiente
+            if self._cabeza:
+                self._cabeza.anterior = None
+            else:
+                self._cola = None
+            self._tamanio -= 1
+            return nodo.dato
+        elif posicion == self._tamanio - 1:
+            nodo = self._cola
+            self._cola = nodo.anterior
+            if self._cola:
+                self._cola.siguiente = None
+            else:
+                self._cabeza = None
+            self._tamanio -= 1
+            return nodo.dato
+        else:
+            actual = self._cabeza
+            for _ in range(posicion):
+                actual = actual.siguiente
+            actual.anterior.siguiente = actual.siguiente
+            actual.siguiente.anterior = actual.anterior
+            self._tamanio -= 1
+            return actual.dato
+
     def copiar(self):
-        if self.size==0:
-            raise ValueError("Lista vacia")
-        nueva_lista = ListaDobleEnlazada()
-        actual = self.cabeza
+        lista_copia = ListaDobleEnlazada()
+        actual = self._cabeza
         while actual is not None:
-            nueva_lista.agregar_al_final(actual.dato)
+            lista_copia.agregar_al_final(actual.dato)
             actual = actual.siguiente
-        return nueva_lista
+        return lista_copia
 
     def invertir(self):
-        actual=self.cabeza
-        aux=None
-        while actual!=None:
-            aux=actual.anterior
-            actual.anterior=actual.siguiente
-            actual.siguiente=aux
-            actual=actual.anterior    
-        if aux!=None:
-            self.cabeza=aux.anterior
-        
+        actual = self._cabeza
+        aux = None
+        while actual is not None:
+            aux = actual.anterior
+            actual.anterior = actual.siguiente
+            actual.siguiente = aux
+            actual = actual.anterior
+        aux = self._cabeza
+        self._cabeza = self._cola
+        self._cola = aux
+
     def concatenar(self, lista_extra):
         if lista_extra.esta_vacia():
             return
+        lista_a_concatenar = lista_extra.copiar()
         if self.esta_vacia():
-            self.cabeza= lista_extra.cabeza
-            self.cola= lista_extra.ultimo
+            self._cabeza = lista_a_concatenar.cabeza
+            self._cola = lista_a_concatenar.cola
         else:
-            self.cola.siguiente = lista_extra.cabeza
-            lista_extra.cabeza.anterior=self.cola
-            self.cola=lista_extra.ultimo
-        self.size+=lista_extra.size
+            lista_a_concatenar.cabeza.anterior = self._cola
+            self._cola.siguiente = lista_a_concatenar.cabeza
+            self._cola = lista_a_concatenar.cola
+            
+        self._tamanio += lista_extra.tamanio
 
     def __add__(self, lista_extra):
-        copia=self.copiar()
+        copia = self.copiar()
         copia.concatenar(lista_extra.copiar())
         return copia
-      
-
-        
-
-                
-
-
-
-
-    # def recorrer_inicio(self):
-    #     aux=self.primero
-    #     while aux != None:
-    #         print(aux.dato)
-    #         aux=aux.siguiente
-
-    # def recorrer_final(self):
-    #     aux=self.ultimo
-    #     while aux:
-    #         print(aux.dato)
-    #         aux=aux.anterior
-
-    # def eliminar_inicio(self):
-    #     if self.vacia():
-    #         print("Lista vacia")
-    #     elif self.primero.siguiente==None:
-    #         self.primero=self.ultimo=None
-    #         self.size=0
-    #     else:
-    #         self.primero=self.primero.siguiente
-    #         self.primero.anterior=None
-    #         self.size-=1
-
-    # def eliminar_final(self):
-    #     if self.vacia():
-    #         print("Lista vacia")
-    #     elif self.primero.siguiente== None:
-    #         self.primero = self.ultimo = None
-    #         self.size=0
-    #     else:
-    #         self.ultimo=self.ultimo.anterior
-    #         self.ultimo.siguiente=None
-    #         self.size-=1
