cf1f39743f9fe154c9a228daf025ac6447929ec7
RocioArener <andrea.aguilar@ingenieria.uner.edu.ar>
2025-04-30 13:28:55 -0300

    agregramos funciones proyecto 3

--- TrabajoPractico_1/proyecto_3/modules/mazo.py
+++ TrabajoPractico_1/proyecto_3/modules/mazo.py
@@ -1,53 +1,32 @@
 # mazo.py
-
-from proyecto_2.modules.modulo1 import ListaDobleEnlazada  # Importa la clase ListaDobleEnlazada
+#C:\Users\alumno\Desktop\AyED2025c1-Cardoso-Arener-Segovia\TrabajoPractico_1\proyecto_2\modules
+from TrabajoPractico_1.proyecto_2.modules.modulo1 import ListaDobleEnlazada  # Importa la clase ListaDobleEnlazada
 from modules.carta import Carta
 
-class DequeEmptyError(Exception):
-    pass  # Define una excepción personalizada para cuando el mazo está vacío
-
-class Mazo:
-    def __init__(self):
-        self.cabeza=None
-        self.ultimo=None
-        self.size=0
-
-    def __iter__(self):
-        nodo_actual = self.cabeza
-        while nodo_actual is not None:
-            yield nodo_actual.dato
-            nodo_actual = nodo_actual.siguiente
-
-    @property
-    def cabeza(self):
-        return self._cabeza
-    
-    @cabeza.setter
-    def cabeza(self, valor):
-        self._cabeza = valor
-    
-    @property
-    def ultimo(self):
-        return self._ultimo
-    
-    @ultimo.setter
-    def ultimo(self, valor):
-        self._ultimo = valor
+class DequeEmptyError(Exception):      
+    def __init__(self, mensaje="Ya no quedan elementos para extraer"):
+        super().__init__(mensaje)
         
-    @property
-    def size(self):
-        return self._size
-    
-    @size.setter
-    def size(self, valor):
-        self._size = valor
+        
+class Mazo():
+    def __init__(self):
+        self.mazo=ListaDobleEnlazada()
 
     def poner_carta_arriba(self, carta):
-        ListaDobleEnlazada.agregar_al_final(carta) 
+        self.mazo.agregar_al_final(carta) 
     
-    def sacar_carta_arriba(self, carta):
-        ListaDobleEnlazada.extraer(self.size-1)
+    def sacar_carta_arriba(self):
+        carta=self.mazo.extraer()
+        carta.visible=True
+        return carta
+        
     
     def poner_carta_abajo(self, carta):
-        ListaDobleEnlazada.agregar_al_inicio(carta)
-    
+        self.mazo.agregar_al_inicio(carta)
+
+if __name__ == '__main__':
+    try:
+        Mazo.sacar_carta_arriba()
+    except:
+        raise DequeEmptyError()
+            
