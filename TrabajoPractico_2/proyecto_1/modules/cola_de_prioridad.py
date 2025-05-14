f37a3d70307324ca05fdf800229a75d449d6bf07
JosefinaCardoso <josefinabelen26@gmail.com>
2025-05-14 13:29:24 -0300

    agregamos funciones

--- TrabajoPractico_2/proyecto_1/modules/cola_de_prioridad.py
+++ TrabajoPractico_2/proyecto_1/modules/cola_de_prioridad.py
@@ -1,14 +1,18 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+import time
+# from modules.paciente import Paciente
+
 class MonticuloBinario:
     def __init__(self):
         self.listaMonticulo=[0]
         self.tamanoActual=0
-        self.raiz=self.listaMonticulo[1]
+    
+
 
     def infiltArriba(self,i):
         while i // 2 > 0:
-            if self.listaMonticulo[i] < self.listaMonticulo[i // 2]:
+            if self.listaMonticulo[i] < self.listaMonticulo[i // 2] :
                 tmp = self.listaMonticulo[i // 2]
                 self.listaMonticulo[i // 2] = self.listaMonticulo[i]
                 self.listaMonticulo[i] = tmp
@@ -23,7 +27,6 @@ class MonticuloBinario:
                 self.listaMonticulo[hm] = tmp
             i = hm
 
-    
     def hijoMin(self,i):
         if i * 2 + 1 > self.tamanoActual:
             return i * 2
@@ -39,9 +42,40 @@ class MonticuloBinario:
         self.infiltArriba(self.tamanoActual)
 
     def eliminarMin(self):
-        valorSacado = self.raiz
-        self.raiz = self.hijoMin(1)
+        valorSacado = self.listaMonticulo[1]
+        self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
         self.tamanoActual = self.tamanoActual - 1
         self.listaMonticulo.pop()
         self.infiltAbajo(1)
         return valorSacado
+    
+    def len(self,lista):
+        self.tamanoActual=len(lista)
+
+    def mostrar(self):
+        return self.listaMonticulo[1:]
+    
+# class Cola_prioridad():
+#     def __init__(self):
+#         self.cola_prioridad=MonticuloBinario()
+
+
+#     def llega_paciente(self,paciente):
+#         if paciente.get_riesgo == 
+#         self.cola_prioridad.insertar(paciente)
+
+#     def atender_paciente(self):
+#         self.cola_prioridad.eliminarMin()
+
+
+if __name__=="__main__":
+   mont= MonticuloBinario()
+   mont.insertar(2)
+   mont.insertar(5)
+   mont.insertar(5)
+   mont.insertar(10)
+   mont.insertar(1)
+   mont.insertar(4)
+   mont.insertar(14)
+   mont.eliminarMin()
+   print(mont.mostrar())
---
+++ TrabajoPractico_2/proyecto_1/modules/paciente.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from random import randint, choices
+import time
+import datetime
+
+nombres = ['Leandro', 'Mariela', 'Gastón', 'Andrea', 'Antonio', 'Estela', 'Jorge', 'Agustina']
+apellidos = ['Perez', 'Colman', 'Rodriguez', 'Juarez', 'García', 'Belgrano', 'Mendez', 'Lopez']
+
+niveles_de_riesgo = [1, 2, 3]
+descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
+# probabilidades de aparición de cada tipo de paciente
+probabilidades = [0.1, 0.3, 0.6] 
+
+class Paciente:
+    def __init__(self,fecha_y_hora):
+        n = len(nombres)
+        self.__nombre = nombres[randint(0, n-1)]
+        self.__apellido = apellidos[randint(0, n-1)]
+        self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
+        self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]   
+        self.__fecha_y_hora=fecha_y_hora
+
+    def get_nombre(self):
+        return self.__nombre
+    
+    def get_hora(self):
+        return self.__fecha_y_hora
+
+    def get_apellido(self):
+        return self.__apellido
+    
+    def get_riesgo(self):
+        return self.__riesgo
+    
+    def get_descripcion_riesgo(self):
+        return self.__descripcion
+    
+    def __str__(self):
+        cad = self.__nombre + ' '
+        cad += self.__apellido + '\t -> '
+        cad += str(self.__riesgo) + '-' + self.__descripcion
+        cad+= "  "+str(self.__fecha_y_hora)
+        return cad
+    def __lt__(self,otro):
+        if self.get_riesgo() < otro.get_riesgo():
+            return self
+        elif self.get_riesgo() == otro.get_riesgo():
+            if self.get_hora() < otro.get_hora():
+                return self
+        else:
+            return otro
+
+    
+if __name__=="__main__":
+    ahora = datetime.datetime.now()
+    p1= Paciente(ahora)
+    time.sleep(5)
+    ahora = datetime.datetime.now()
+    p2= Paciente(ahora)
+    print(p1)
+    print(p2)
+    if p1>p2:
+        print ("hola")
+        print(p1)
+
+    else:
+        print("chau")
+        print(p2)
+    print(p1.get_hora())
+        
+        
